e256031f6e3a314385fd1df744bd173d4520e5fa
NahidHasan131 <nahid7532140@gmail.com>
2025-12-24 16:15:28 +0600

    loop

---
+++ loop.js
@@ -0,0 +1,19 @@
+//loop
+// const fruits = ['apple', 'banana', 'cherry'];
+// for(const fruit of fruits){
+//     console.log(fruit);
+// }
+
+
+let num = 1;
+while(num <= 5){
+    console.log(`The number is: ${num}`);
+    num++;
+}
+
+
+
+
+
+
+

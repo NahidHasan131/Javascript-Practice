82b449e5b3cc2678e79517af56255ea9a5ee00cd
NahidHasan131 <nahid7532140@gmail.com>
2025-12-27 22:44:17 +0600

    truthy,falsy value

---
+++ truthy, false ternary operator.js
@@ -0,0 +1,43 @@
+// const test = 0
+
+// if(test){
+//     console.log("Its truthy value")
+// }else{
+//     console.log("Its falsy value")
+// }
+
+// //ternery truthy or falsy
+// test ? console.log("Truthy value") : console.log("Falsy value")
+
+
+
+// const num = 10
+// const result = num>= 10 && num <= 50 ? true : false
+// console.log(result)
+
+
+
+// const isActive = true
+// console.log(!isActive)
+
+
+const isActive = true
+
+const showUser = ()=> console.log("Show user Details")
+const HideUser = ()=> console.log("Hide user Details")
+
+// isActive ? showUser() : HideUser()
+
+// isActive && showUser()
+// isActive || HideUser()
+
+
+
+
+
+
+
+
+
+
+
